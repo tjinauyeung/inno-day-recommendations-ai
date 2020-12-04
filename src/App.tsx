8d3b5732862907a4b192b54c8134389de5a59869
Tjin Au Yeung <tjinauyeung@gmail.com>
2020-12-04 14:14:33 +0100

    add categories

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,25 @@ import products from "../products.json";
 interface IProduct {
   category: string[];
   color: string;
-  id: number;
   image: string;
   name: string;
   price: string;
   sku: number;
 }
 
-const App = () => (
-  <div className="p-24 min-h-screen bg-gray-900">
-    {/* <section className="my-12">
+const CATEGORIES = [
+  { label: "Shirt", value: "Shirt" },
+  { label: "Pants", value: "Pant" },
+  { label: "Shoes", value: "Shoe" },
+];
+
+const App = () => {
+  const [category, setCategory] = React.useState("Shoe");
+
+  return (
+    <div className="p-24 min-h-screen bg-gray-900">
+      <div className="container mx-auto">
+        {/* <section className="my-12">
       <Heading>Just for you</Heading>
       <Grid>
         {products.map((product) => (
@@ -23,74 +32,93 @@ const App = () => (
       </Grid>
     </section> */}
 
-    <section className="my-12">
-      <Heading>Products</Heading>
-      <Grid>
-        {products.map((product) => (
-          <Product key={product.sku} product={product} />
-        ))}
-      </Grid>
-    </section>
-  </div>
-);
+        <section className="my-12">
+          <Heading>The Stinged</Heading>
+
+          {CATEGORIES.map((c) => (
+            <button
+              key={c.value}
+              className="text-white rounded mr-4 focus:outline-none"
+              onClick={() => setCategory(c.value)}
+            >
+              <Subheading active={c.value === category}>{c.label}</Subheading>
+            </button>
+          ))}
+
+          <Grid>
+            {products
+              .filter((p) => p.category.includes(category))
+              .map((product) => (
+                <Product key={product.sku} product={product} />
+              ))}
+          </Grid>
+        </section>
+      </div>
+    </div>
+  );
+};
 
 const Heading = ({ children }) => (
-  <h1 className="text-4xl mb-4 font-bold text-white capitalize">{children}</h1>
+  <h1 className="text-6xl mb-4 text-white font-bold capitalize">{children}</h1>
 );
+
+const Subheading = ({ children, active }) => (
+  <h2
+    className="text-4xl mt-24 text-white mb-8 font-bold capitalize"
+    style={{ opacity: active ? 1 : 0.5 }}
+  >
+    {children}
+  </h2>
+);
+
 const Grid = ({ children }) => (
   <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
     {children}
   </div>
 );
 
-const Product: FC<{ product: IProduct }> = ({ product }) => (
-  <div className="shadow rounded-lg p-6 bg-white">
-    <h1 className="text-3xl font-bold">{product.name}</h1>
-    <img src={product.image} />
-    <div className="flex justify-between items-end mt-12">
-      <div>
-        <Button
-          onClick={() =>
-            window.dataLayer.push({
-              event: "like-product",
-            })
-          }
-        >
-          👍
-        </Button>
-        <Button
-          onClick={() =>
-            window.dataLayer.push({
-              event: "dislike-product",
-            })
-          }
-        >
-          👎
-        </Button>
+const Product: FC<{ product: IProduct }> = ({ product }) => {
+  const like = (data) => () => {
+    console.log("[event-emitted]: 'like-product'");
+    console.log(JSON.stringify(data, null, 2));
+    window.dataLayer.push({
+      event: "like-product",
+      data,
+    });
+  };
+  const dislike = (data) => () => {
+    console.log("[event-emitted]: 'dislike-product'");
+    console.log(JSON.stringify(data, null, 2));
+    window.dataLayer.push({
+      event: "dislike-product",
+      data,
+    });
+  };
+  return (
+    <div className="shadow rounded-2xl p-6 bg-white opacity-90 flex flex-col">
+      <h1 className="text-xl font-bold">{product.name}</h1>
+      <img src={product.image} className="flex-1 h-48 m-auto" />
+      <div className="flex justify-between items-end">
+        <div>
+          <Button onClick={like(product)}>😍</Button>
+          <Button onClick={dislike(product)}>😢</Button>
+        </div>
+        <p className="text-xl font-bold font-mono text-right">
+          {/* {formatPrice(product.price)} */}
+          {product.price}
+        </p>
       </div>
-      <p className="text-2xl font-bold text-green-700 font-mono text-right">
-        {/* {formatPrice(product.price)} */}
-        {product.price}
-      </p>
     </div>
-  </div>
-);
+  );
+};
 
 const Button = ({ children, onClick }) => (
   <button
-    className="rounded-full w-10 h-10 shadow mr-2 focus:ring focus:outline-none"
+    className="rounded-full w-12 h-12 shadow mr-2 text-3xl focus:ring focus:outline-none"
     onClick={onClick}
   >
     {children}
   </button>
 );
-
-const formatPrice = (price) => {
-  const formatter = new Intl.NumberFormat("en", {
-    style: "currency",
-    currency: "EUR",
-  });
-  return formatter.format(price);
-};
 
 export default App;
