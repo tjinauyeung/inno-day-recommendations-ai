c5b1f5e2ef850fa7d2bc9e0dc962e63955d19cea
Tjin Au Yeung <tjinauyeung@gmail.com>
2021-02-26 12:19:23 +0100

    add event time to user events

--- generate.ts
+++ generate.ts
@@ -1,14 +1,18 @@
 import axios from "axios";
 
+const randomId = () => Math.round(Math.random() * 40);
+
 const sendRequest = () => {
+    const visitorId = randomId();
+    const userId = randomId();
     axios({
         method: "post",
         url: "https://recommendationengine.googleapis.com/v1beta1/projects/xsd-recommendation-ai/locations/global/catalogs/default_catalog/eventStores/default_event_store/userEvents:write?key=AIzaSyC6iyTSdcvcWAcnIoew44OI__gdWlwIJNs",
         data: {
             'eventType': 'detail-page-view',
             'userInfo': {
-                'visitorId': '19',
-                'userId': '12',
+                'visitorId': String(visitorId),
+                'userId': String(userId),
                 'ipAddress': '0.0.0.0',
                 'userAgent': 'Mozilla/5.0 (Windows NT 6.1)'
             },
@@ -22,7 +26,8 @@ const sendRequest = () => {
                     'displayPrice': 130.00,
                     'currencyCode':'USD'
                 }]
-            }
+            },
+            'eventTime': '2021-01-26T11:11:40.143685881Z'
         }
     }).then((response) => {
         console.log(response.status);
